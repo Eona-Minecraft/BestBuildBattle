89163c6046cade34bca062012070112f541cf2d5
Nuss9940 <nuss9940@gmail.com>
2015-06-16 01:52:30 +0200

    Init

---
+++ Unloader/src/de/nuss9940/unloader/Unloader.java
@@ -0,0 +1,211 @@
+package de.nuss9940.unloader;
+
+import java.util.Calendar;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.onarandombox.MultiverseCore.MultiverseCore;
+
+
+public class Unloader extends JavaPlugin {
+	
+	
+	//Varc
+	String world = "Creative1";
+	String message1 = ChatColor.RED + "Creative wird in " + ChatColor.GREEN;
+	String message2 = ChatColor.RED + "Minuten geschlossen!";
+	
+	//Var
+	//private static final Unloader instance = new Unloader();
+	public static Unloader plugin;
+	//public Unloader(Unloader plugin) {  Unloader.plugin = plugin;  }
+	MultiverseCore mv = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("MultiverseCore");
+	Logger log = Logger.getLogger("Minecraft");
+	
+	
+	//depend: [MultiverseCore]
+	
+	
+	public void onDisable(){
+		
+		plugin = null;
+		
+	}
+	
+	
+	public void onEnable(){
+		
+		
+		//Var
+		plugin = this;
+		Calendar c = Calendar.getInstance();
+		int hh = c.get(Calendar.HOUR_OF_DAY);
+		int day = c.get(Calendar.DAY_OF_WEEK);
+		int unloadtime;
+		
+		//Day
+		if (day == Calendar.FRIDAY || day == Calendar.SATURDAY) {
+			unloadtime = 23;
+		} else {
+			unloadtime = 22;
+		}
+		
+		if (hh >= unloadtime || hh < 9) {
+			
+			//unload
+			unload(getTime());
+			
+		} else if (hh < unloadtime || hh >= 9) {
+			
+			//load
+			load(getTime());
+			
+		}
+		
+	}
+	
+	
+	public long getTime() {
+		
+		
+		//Var
+		Calendar c = Calendar.getInstance();
+		
+		int hh = c.get(Calendar.HOUR_OF_DAY);
+		int mm = c.get(Calendar.MINUTE);
+		int day = c.get(Calendar.DAY_OF_WEEK);
+		int unloadtime;
+		int whh = 0;
+		int whhinmm = 0;
+		
+		log.info("[Unloader] System Time: " + hh + ":" + mm);
+		
+		//Day
+		if (day == Calendar.FRIDAY || day == Calendar.SATURDAY) {
+			
+			unloadtime = 23;
+			
+		} else {
+			
+			unloadtime = 22;
+			
+		}
+		
+		
+		//Calc
+		if (hh >= unloadtime || hh < 9) {
+			
+			//unload
+			if (hh > 9) {
+				
+				whh = 24 - hh + 9;
+				
+			} else {
+				
+				whh = 9 - hh;
+				
+			}
+			
+			
+			whhinmm = whh * 60;
+			
+			log.info("[Unloader] Full hours until unload: " + whh + " - equals " + whhinmm + " Minutes!");
+			log.info("[Unloader] Minutes to subtract: " + mm);
+			
+			
+		} else if (hh < unloadtime || hh >= 9) {
+			
+			//load
+			
+			whh = unloadtime - hh;
+			
+			whhinmm = whh * 60;
+			
+			log.info("[Unloader] Full hours until load: " + whh + " - equals " + whhinmm + " Minutes!");
+			log.info("[Unloader] Minutes to subtract: " + mm);
+			
+			
+		} else {
+			log.warning("[Unloader] Could not read system time!");
+			log.warning("[Unloader] World will not be unloaded!");
+			return -1;
+		}
+		
+		
+		return whhinmm - mm - 5;//-5 for 5m message before
+		
+	}
+	
+	
+	public void unload(long t) {
+		
+		
+		Bukkit.getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				Bukkit.broadcastMessage(ChatColor.RED + "Die Creative Welt wird in " + ChatColor.GREEN + "5 Minuten" + ChatColor.RED + " geschlossen!");
+				
+				Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					
+					@Override
+					public void run() {
+						
+						Bukkit.broadcastMessage(ChatColor.RED + "Die Creative Welt wird " + ChatColor.GREEN + "JETZT" + ChatColor.RED + " geschlossen!");
+						
+						mv = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("MultiverseCore");
+						mv.getCore().getMVWorldManager().unloadWorld(world, true);
+						
+						Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+							
+							@Override
+							public void run() { load(getTime()); }}, 1800L);//1.5 min
+						
+					}
+				}, 6000L);//5 min
+				
+			}
+		}, t);
+		
+	}
+	
+	
+	
+	public void load(long t) {
+		
+		
+		Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				Bukkit.broadcastMessage(ChatColor.RED + "Die Creative Welt wird in " + ChatColor.GREEN + "5 Minuten" + ChatColor.RED + " geöffnet!");
+				
+				Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					
+					@Override
+					public void run() {
+						
+						Bukkit.broadcastMessage(ChatColor.RED + "Die Creative Welt wird " + ChatColor.GREEN + "JETZT" + ChatColor.RED + " geöffnet!");
+						mv = (MultiverseCore) Bukkit.getServer().getPluginManager().getPlugin("MultiverseCore");
+						mv.getCore().getMVWorldManager().loadWorld(world);
+						
+						Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+							
+							@Override
+							public void run() { unload(getTime()); }}, 1800L);//1.5 min
+						
+					}
+				}, 6000L);//5 min
+				
+			}
+		}, t);
+		
+	}
+	
+	
+}
